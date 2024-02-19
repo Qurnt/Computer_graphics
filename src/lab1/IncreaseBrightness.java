bcd3fe354ec958ffc9c006d880c6b93bb5526b43
Aleksandr Marov <sasha_marov@list.ru>
2024-02-19 23:18:24 +0300

    first comm

---
+++ src/lab1/IncreaseBrightness.java
@@ -0,0 +1,53 @@
+package lab1;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+class IncreaseBrightness {
+    public static int Truncate(int value) {WW
+        if (value < 0) {
+            value = 0;
+        } else if (value > 255) {
+            value = 255;
+        }
+        return value;
+    }
+
+    public static void AdjustBrightness(String inpPath, String outPath) throws IOException {
+        File f = new File(inpPath);
+        BufferedImage image = ImageIO.read(f);
+
+        int[] rgb;
+
+        // ЗДЕСЬ МЕНЯЕМ ЯРКОСТЬ.
+        int brightnessValue = 25;
+
+        // Outer loop for width of image
+        for (int i = 0; i < image.getWidth(); i++) {
+
+            for (int j = 0; j < image.getHeight(); j++) {
+
+                rgb = image.getRaster().getPixel(i, j, new int[3]);
+
+                int red = Truncate(rgb[0] + brightnessValue);
+                int green = Truncate(rgb[1] + brightnessValue);
+                int blue = Truncate(rgb[2] + brightnessValue);
+
+                int[] arr = {red, green, blue};
+
+                image.getRaster().setPixel(i, j, arr);
+            }
+        }
+
+        ImageIO.write(image, "jpg", new File(outPath));
+    }
+
+    public static void main(String[] args) throws Exception {
+        String inputPath = "src/lab1/input/lab1.jpg";
+        String outputPath = "src/lab1/output/lab1_brightness.jpg";
+
+        AdjustBrightness(inputPath, outputPath);
+    }
+}
